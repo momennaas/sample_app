88269c35165b6fd73873f069499cff2d5f589663
Momen Naas <momennaas@gmail.com>
2016-09-05 22:14:53 +0000

    finish static pages

--- app/controllers/page_controller_controller.rb
+++
@@ -1,7 +0,0 @@
-class PageControllerController < ApplicationController
-  def home
-  end
-
-  def help
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'page_controller/home'
-
-  get 'page_controller/help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#index'
+  get 'pages/home'
+  get 'pages/help'
+  get 'pages/about'
+  get 'pages/contact'
+  
+  root 'pages#home'
 end
